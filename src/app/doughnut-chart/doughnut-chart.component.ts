16be06e57e0bab3e4f2f7dedf6c0de9ab7d98034
LiNo <castlino@gmail.com>
2020-05-31 11:30:20 +1000

    Added dynamic doughnut chart data and fixed issues not updating...

--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -10,25 +10,57 @@ import { CasesService } from '../services/cases.service';
 })
 
 export class DoughnutChartComponent {
-
   doughnutChartLabels: Label[] = ['Other', 'gardening', 'Defects', 'Renovations', 'Cleaning', 'Repair & Maintenance'];
   doughnutChartData: MultiDataSet = [ [0, 0, 0, 0, 0, 0] ];
   doughnutChartType: ChartType = 'doughnut';
   doughnutChartLegend: false;
   
+  chart: any;
+  
+  mySubscription: any;
+  
+  public doughnutChartOptions = {
+    legend: {
+      display: false
+    }
+  };
+  
+  public doughnutChartColors: Array<any> = [
+    { 
+      backgroundColor: [
+        'rgba(254, 11, 48, 1)',
+        'rgba(86, 223, 144, 1)',
+        'rgba(186, 223, 86, 1)',
+        'rgba(191, 86, 223, 1)',
+        'rgba(235, 5, 5, 1)',
+        'rgba(43, 203, 186, 1)',
+        'rgba(5, 235, 235, 1)',
+        'rgba(169, 86, 223, 1)',
+        'rgba(252, 92, 101, 1)',
+        'rgba(86, 160, 223, 1)',
+        'rgba(102, 86, 223, 1)',
+        'rgba(223, 86, 218, 1)'
+      ]
+    }
+  ];
+  
   timeFrame: string;
   
   constructor(
     private caseService: CasesService
-  ) { }
+  ) { 
+    
+  }
   
   ngOnInit(): void {  
     this.timeFrame = '-1 month';
     this.getCaseTypeStats();
   }
   
+  ngAfterViewInit() { }
+  
   getCaseTypeStats(): void {
-    this.caseService.getCaseTypeStats(this.timeFrame = '-1 month')
+    this.caseService.getCaseTypeStats( this.timeFrame )
         //.pipe( delay(1000) )  // test loader display by adding delay.
         .subscribe(
           caseTypeStats => {
@@ -42,5 +74,6 @@ export class DoughnutChartComponent {
   public onTimeFrameChange() {
     this.getCaseTypeStats();
   }
+  
 
 }
